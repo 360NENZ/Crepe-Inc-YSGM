a31a9bd03903e28087a0293b972dc2c18dde45ac
memetrollsXD <memetrollsxd@gmail.com>
2022-10-28 14:29:10 +0200

    Hand over skill issue

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using YSGM;
+
+Console.WriteLine(@"
+██╗   ██╗███████╗ ██████╗ ███╗   ███╗
+╚██╗ ██╔╝██╔════╝██╔════╝ ████╗ ████║
+ ╚████╔╝ ███████╗██║  ███╗██╔████╔██║
+  ╚██╔╝  ╚════██║██║   ██║██║╚██╔╝██║
+   ██║   ███████║╚██████╔╝██║ ╚═╝ ██║
+   ╚═╝   ╚══════╝ ╚═════╝ ╚═╝     ╚═╝
+                                     
+");
+Console.ReadKey();
+
+SSHManager.Instance.Init();
---
+++ SSHManager.cs
@@ -0,0 +1,41 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YSGM
+{
+    public class SSHManager
+    {
+        public SshClient client;
+        public static SSHManager Instance = new();
+
+        private SSHManager()
+        {
+#if DEBUG
+            string? host = "hk4e-storage.mihoyo.com";
+            string? user = "root";
+            string? keyPath = @"C:\Users\Paris\.ssh\id_rsa";
+#else
+            string? host = ConfigurationManager.AppSettings.Get("SSH_HOST");
+            string? user = ConfigurationManager.AppSettings.Get("SSH_USER");
+            string? keyPath = ConfigurationManager.AppSettings.Get("SSH_KEY_PATH");
+#endif
+            // Read the private key file
+            PrivateKeyFile keyFile = new(keyPath);
+            var keyFiles = new[] { keyFile };
+            PrivateKeyAuthenticationMethod authMethod = new(user, keyFiles);
+            ConnectionInfo connectionInfo = new (host, user, authMethod);
+
+            client = new SshClient(connectionInfo);
+        }
+
+        public void Init()
+        {
+            client.Connect();
+        }
+    }
+}
